521c15ff1410ce306897dffda64c46bb0d53b7c2
Aleš Trunda <alestrunda@gmail.com>
2019-12-24 19:12:45 +0100

    code cleaning

--- src/components/PercentDifference.tsx
+++ src/components/PercentDifference.tsx
@@ -4,6 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown, faArrowUp, faMinus } from "@fortawesome/free-solid-svg-icons";
 import { PERCENTAGE_MARGIN } from "../constants";
 
+enum Type {
+  down = "down",
+  mid = "mid",
+  up = "up"
+}
+
 interface Props {
   className?: string;
   value1: number;
@@ -13,16 +19,20 @@ interface Props {
 const PercentDifference = ({ className, value1, value2 }: Props) => {
   const difference = Math.round(-((value1 / value2) * 100 - 100));
   const percentageType =
-    Math.abs(difference) <= PERCENTAGE_MARGIN ? "mid" : difference >= 0 ? "up" : "down";
+    Math.abs(difference) <= PERCENTAGE_MARGIN ? Type.mid : difference >= 0 ? Type.up : Type.down;
 
   return (
     <span className={classNames("percentage", className, `percentage--${percentageType}`)}>
-      {percentageType === "down" && (
+      {percentageType === Type.down && (
         <FontAwesomeIcon className="percentage__icon" icon={faArrowDown} />
       )}
-      {percentageType === "mid" && <FontAwesomeIcon className="percentage__icon" icon={faMinus} />}
-      {percentageType === "up" && <FontAwesomeIcon className="percentage__icon" icon={faArrowUp} />}
-      {percentageType === "up" && "+"}
+      {percentageType === Type.mid && (
+        <FontAwesomeIcon className="percentage__icon" icon={faMinus} />
+      )}
+      {percentageType === Type.up && (
+        <FontAwesomeIcon className="percentage__icon" icon={faArrowUp} />
+      )}
+      {percentageType === Type.up && "+"}
       {difference}%
     </span>
   );
