26b62d35831354da191c91c13fdce9579467e49c
Aleš Trunda <alestrunda@gmail.com>
2020-04-18 15:07:17 +0200

    chore: prettier

--- src/components/AutocompleteItem.tsx
+++ src/components/AutocompleteItem.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import classNames from "classnames";
 
 interface Props {
-  className?: string,
-  id: string,
-  onSelect(id: string, name: string): void,
-  title: string,
+  className?: string;
+  id: string;
+  onSelect(id: string, name: string): void;
+  title: string;
 }
 
 const AutocompleteItem = ({ className, id, onSelect, title }: Props) => (
-  <li className={classNames("item-stripes", className)} onClick={() => onSelect(id, title)}>{title}</li>
-)
+  <li className={classNames("item-stripes", className)} onClick={() => onSelect(id, title)}>
+    {title}
+  </li>
+);
 
 AutocompleteItem.defaultProps = {
   className: ""
-}
+};
 
-export default AutocompleteItem
+export default AutocompleteItem;
--- src/interfaces/Subject.tsx
+++ src/interfaces/Subject.tsx
@@ -1,4 +1,4 @@
 export default interface Subject {
-  _id: string,
-  name: string,
+  _id: string;
+  name: string;
 }
