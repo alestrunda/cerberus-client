e52e5967cb5e6447e532fa9e0fc272cbbcfbd98f
Aleš Trunda <alestrunda@gmail.com>
2020-10-04 15:09:29 +0200

    refactor: code cleaning

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -94,118 +94,116 @@ const Homepage = () => {
             isLoading={loading}
             styleOverlay={{ height: "auto", bottom: 20 }}
           >
-            <>
-              <div className="grid">
-                <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
-                  <div className="el-full el-relative container-flex">
-                    <Link to="/debt/new/" className="label-abs">
-                      <FontAwesomeIcon icon={faPlus} />
-                    </Link>
-                    <Link to="/debts/" className="box box--debt box--center ">
-                      <div className="box__content box__content--big">
-                        <h2>
-                          Debts
-                          <br />
-                          {debts.length}
-                        </h2>
-                        <p>
-                          <strong>{debtsNotPaid.length}</strong> not paid
-                        </p>
-                        {latestDebt && (
-                          <p>
-                            latest: <i>{latestDebt.subject.name}</i> for{" "}
-                            <Price className="text-bold">{latestDebt.amount}</Price>
-                          </p>
-                        )}
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-                <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
-                  <div className="el-full el-relative container-flex">
-                    <Link to="/income/new/" className="label-abs">
-                      <FontAwesomeIcon icon={faPlus} />
-                    </Link>
-                    <Link to="/incomes/" className="box box--income box--center">
-                      <div className="box__content box__content--big">
-                        <h2>
-                          Incomes
-                          <br />
-                          {incomes.length}
-                        </h2>
-                        {latestIncome && (
-                          <p>
-                            latest: <i>{latestIncome.subject.name}</i> for{" "}
-                            <Price className="text-bold">{latestIncome.amount}</Price>
-                          </p>
-                        )}
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-                <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
-                  <div className="el-full el-relative container-flex">
-                    <Link to="/outlay/new/" className="label-abs">
-                      <FontAwesomeIcon icon={faPlus} />
-                    </Link>
-                    <Link to="/outlays/" className="box box--outlay box--center">
-                      <div className="box__content box__content--big">
-                        <h2>
-                          Outlays
-                          <br />
-                          {outlays.length}
-                        </h2>
-                        {latestOutlay && (
-                          <p>
-                            latest: <i>{latestOutlay.subject.name}</i> for{" "}
-                            <Price className="text-bold">{latestOutlay.amount}</Price>
-                          </p>
-                        )}
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-                <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
-                  <Link to="/stats/" className="box box--debt box--center">
+            <div className="grid">
+              <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
+                <div className="el-full el-relative container-flex">
+                  <Link to="/debt/new/" className="label-abs">
+                    <FontAwesomeIcon icon={faPlus} />
+                  </Link>
+                  <Link to="/debts/" className="box box--debt box--center ">
                     <div className="box__content box__content--big">
-                      <h2>Debts Total</h2>
+                      <h2>
+                        Debts
+                        <br />
+                        {debts.length}
+                      </h2>
                       <p>
                         <strong>{debtsNotPaid.length}</strong> not paid
                       </p>
-                      <p>
-                        total <Price className="text-bold">{debtTotal}</Price>
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-                <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
-                  <Link to="/stats/" className="box box--income box--center">
-                    <div className="box__content box__content--big">
-                      <h2>Incomes difference</h2>
-                      <p>
-                        last year <Price className="text-bold">{incomesTotalLastYear}</Price>
-                      </p>
-                      <p>
-                        this year <Price className="text-bold">{incomesTotalThisYear}</Price>
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-                <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
-                  <Link to="/stats/" className="box box--outlay box--center">
-                    <div className="box__content box__content--big">
-                      <h2>Outlays difference</h2>
-                      <p>
-                        last year <Price className="text-bold">{outlaysTotalLastYear}</Price>
-                      </p>
-                      <p>
-                        this year <Price className="text-bold">{outalysTotalThisYear}</Price>
-                      </p>
+                      {latestDebt && (
+                        <p>
+                          latest: <i>{latestDebt.subject.name}</i> for{" "}
+                          <Price className="text-bold">{latestDebt.amount}</Price>
+                        </p>
+                      )}
                     </div>
                   </Link>
                 </div>
               </div>
-            </>
+              <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
+                <div className="el-full el-relative container-flex">
+                  <Link to="/income/new/" className="label-abs">
+                    <FontAwesomeIcon icon={faPlus} />
+                  </Link>
+                  <Link to="/incomes/" className="box box--income box--center">
+                    <div className="box__content box__content--big">
+                      <h2>
+                        Incomes
+                        <br />
+                        {incomes.length}
+                      </h2>
+                      {latestIncome && (
+                        <p>
+                          latest: <i>{latestIncome.subject.name}</i> for{" "}
+                          <Price className="text-bold">{latestIncome.amount}</Price>
+                        </p>
+                      )}
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
+                <div className="el-full el-relative container-flex">
+                  <Link to="/outlay/new/" className="label-abs">
+                    <FontAwesomeIcon icon={faPlus} />
+                  </Link>
+                  <Link to="/outlays/" className="box box--outlay box--center">
+                    <div className="box__content box__content--big">
+                      <h2>
+                        Outlays
+                        <br />
+                        {outlays.length}
+                      </h2>
+                      {latestOutlay && (
+                        <p>
+                          latest: <i>{latestOutlay.subject.name}</i> for{" "}
+                          <Price className="text-bold">{latestOutlay.amount}</Price>
+                        </p>
+                      )}
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
+                <Link to="/stats/" className="box box--debt box--center">
+                  <div className="box__content box__content--big">
+                    <h2>Debts Total</h2>
+                    <p>
+                      <strong>{debtsNotPaid.length}</strong> not paid
+                    </p>
+                    <p>
+                      total <Price className="text-bold">{debtTotal}</Price>
+                    </p>
+                  </div>
+                </Link>
+              </div>
+              <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
+                <Link to="/stats/" className="box box--income box--center">
+                  <div className="box__content box__content--big">
+                    <h2>Incomes difference</h2>
+                    <p>
+                      last year <Price className="text-bold">{incomesTotalLastYear}</Price>
+                    </p>
+                    <p>
+                      this year <Price className="text-bold">{incomesTotalThisYear}</Price>
+                    </p>
+                  </div>
+                </Link>
+              </div>
+              <div className="grid__item grid__item--lg-span-4 grid__item--md-span-6 container-flex">
+                <Link to="/stats/" className="box box--outlay box--center">
+                  <div className="box__content box__content--big">
+                    <h2>Outlays difference</h2>
+                    <p>
+                      last year <Price className="text-bold">{outlaysTotalLastYear}</Price>
+                    </p>
+                    <p>
+                      this year <Price className="text-bold">{outalysTotalThisYear}</Price>
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            </div>
           </SectionLoad>
         </div>
       </main>
