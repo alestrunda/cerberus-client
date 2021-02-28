b4e39bc5f37bf8987faa9c52112a02b3c6fe4fba
Aleš Trunda <alestrunda@gmail.com>
2021-02-28 19:37:26 +0100

    mock i18 in tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom";
+
+//mock react-i18next so we won't get warning about it not being initialized
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key) => key })
+}));
