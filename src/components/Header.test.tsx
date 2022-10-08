08a1ed075623d2e49907dd45b5468179677079d3
Aleš Trunda <alestrunda@gmail.com>
2022-10-08 17:13:04 +0200

    update packages

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,15 +1,12 @@
 import { render } from "@testing-library/react";
-import { Router } from "react-router-dom";
-import { createMemoryHistory } from "history";
+import { MemoryRouter } from "react-router-dom";
 import Header from "./Header";
-
-const history = createMemoryHistory();
 
 it("renders title with link to the homepage", () => {
   const { container } = render(
-    <Router history={history}>
+    <MemoryRouter>
       <Header />
-    </Router>
+    </MemoryRouter>
   );
   expect(container.querySelector("a.page-head__title")).toHaveAttribute("href", "/");
   expect(container.querySelector("h1")).toHaveTextContent("Cerberus");
@@ -17,9 +14,9 @@ it("renders title with link to the homepage", () => {
 
 it("renders a link in navigation", () => {
   const { container } = render(
-    <Router history={history}>
+    <MemoryRouter>
       <Header />
-    </Router>
+    </MemoryRouter>
   );
   expect(container.querySelectorAll(".nav-main a").length).toBeGreaterThan(0);
 });
